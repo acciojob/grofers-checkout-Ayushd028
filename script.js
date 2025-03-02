f79f10d8c5561d87ed7959cae9c7e234463a13b2
Ayush Dwivedi <dwivediayush028@gmail.com>
2025-03-02 13:00:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ const getSum = () => {
 	const prices = document.querySelectorAll(".price");
 	let sum = 0;
 	prices.forEach((item)=>{
-		sum += parseFloat(item.textContent) || 0;
+	 sum += parseFloat(item.textContent) || 0;
 	})
-	table.innerHTML += `<tr><td colspan = "2">Total Price: ${sum}</td></tr>`
+	table.innerHTML += `<tr id="ans"><td colspan = "2">Total Price: ${sum}</td></tr>`
 };
 
 getSumBtn.addEventListener("click", getSum);
